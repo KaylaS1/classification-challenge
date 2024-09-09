9a80da61bb148f1580bfb93067b431cb283635d8
KaylaS1 <kalas1@hotmail.com>
2024-09-09 18:07:08 -0400

    Updated README, pipeline_utilities.py and retested updated code

--- README.md
+++ README.md
@@ -5,16 +5,21 @@
 ### Step 1:
 * Read in the spam data csv file and create a dataframe.
 * Break out dataframe into training and testing.
-* Scale the features.
+* I wrote code in my pipeline_utilities Python file to scale the X_train and X_test features.
 
 ### Initial Prediction
 * I think Logistic Regression will perform better because spam has more of features like spelling mistakes, poor grammar, capitals, etc. So I would think the spam versus non-spam groups or clusters could be separated by a straight line. Also dataset looks somewhat balanced and it is not huge. Spam classification decision-making is not too complex for Logistic Regression.
 
 ### Step 2: Develop and fit a Logistic Regression model
-* TBD
+* I wrote the Logistic Regression model code in my pipeline_utilities Python file.
+* I sent in the scaled data as arguments.
+* The Logistic Regression model had an accuracy rate of 93.22%.
 
 ### Step 3: Develop and fit a Random Forest Classifier model
-* In progress
+* I wrote the Random Forest model code in my pipeline_utilities Python file.
+* I used the default number of 100 estimators and sent in the scaled data as arguments.
+* The Random Forest CLassifier model had an accuracy rate of 95.22%.
 
 ### Conclusions:
-* Both models had over 90% accuracy score, indicating that both models performed reasonably well, but the Random Forest model performed better than the Logistic Regression model. The Random Forest model achieved an accuracy of 95.65% with testing data and predictions while the Logistic Regression model achieved a score of 92.96%.
+* Both models had over 90% accuracy score, indicating that both models performed reasonably well, but the Random Forest model performed better than the Logistic Regression model by 2 percentage points. The Random Forest model achieved an accuracy of 95.22% with testing data and predictions while the Logistic Regression model achieved a score of 93.22%.
+* My prediction that the Logistic Regression model would perform better was incorrect!
--- pipeline_utilities.py
+++ pipeline_utilities.py
@@ -1,26 +1,63 @@
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.linear_model import LinearRegression, Ridge, LogisticRegression
-from sklearn.metrics import mean_squared_error, r2_score, accuracy_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 from sklearn.model_selection import train_test_split
 from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.preprocessing import StandardScaler
 import pandas as pd
+
+def scale_data_with_StandardScaler(X_train, X_test):
+    """
+    Scales input data using Standard Scaler.
+    Returns scaled DataFrames of both input dfs
+    """
+    # Create the StandardScaler instance
+    scaler = StandardScaler()
+
+    # Fit the Standard Scaler with the training data
+    scaler.fit(X_train)
+
+    # Scale the training data
+    X_train_scaled = scaler.transform(X_train)
+    print(f"Scaled X_train data: {X_train_scaled}")
+
+    # Transforming the test dataset based on the fit from the training dataset
+    X_test_scaled = scaler.transform(X_test)
+    print(f"Scaled X_train data: {X_test_scaled}")
+
+    # Return the scaled data
+    return X_train_scaled, X_test_scaled
 
 def logistic_regression_model_generator(X_train, X_test, y_train, y_test, r_state):
     """
-    Kala document this function.
+    Generates and fits a Logistic Regression model.
+    Uses training and testing data passed in as arguments.
+    Makes predictions using testing data and prints model accuracy score.
+    Does not return anything.
     """
     model = LogisticRegression(random_state=r_state)
+
+    # Train a Logistic Regression model and print the model score
     model.fit(X_train, y_train)
     print(f"Logistic Regression Training Data Score: {model.score(X_train, y_train)}")
     print(f"Logistic Regression Testing Data Score: {model.score(X_test, y_test)}")
+    
+    # Make and save testing predictions with the saved logistic regression model using the test data
     predictions = model.predict(X_test)
+    
+    # Review the predictions
     print(f"Logistic Regression Predictions: {predictions}")
+
+    # Calculate the accuracy score by evaluating `y_test` vs. `testing_predictions`
     print(f"Logistic Regression Predictions: {accuracy_score(y_test, predictions)}")
 
 def random_forest_model_generator(X_train, X_test, y_train, y_test, r_state, estimator_count, X_columns):
     """
-    Kala document this function.
+    Generates and fits a Random Forest model.
+    Uses training and testing data passed in as arguments.
+    Makes predictions using testing data.
+    Prints model accuracy score and top 10 most important features.
+    Does not return anything.
     """
     # Create the random forest classifier instance
     model = RandomForestClassifier(n_estimators=estimator_count, random_state=r_state)
@@ -44,111 +81,6 @@ def random_forest_model_generator(X_train, X_test, y_train, y_test, r_state, est
     importances_sorted = sorted(zip(model.feature_importances_, X_columns), reverse=True)
     importances_sorted[:10]
 
-def split_train_test(data_df, target_col):
-    """
-    Splits input data into training and testing sets.
-    Uses target column to create labels set `y` and removes it from features DataFrame `X`.
-    Returns training and testing data sets.
-    """
-    y = data_df[target_col].values.reshape(-1, 1)
-    X = data.copy().drop(columns=target_col)
-    return train_test_split(X, y)
-
-def r2_adj(x, y, model):
-    """
-    Calculates adjusted r-squared values given an X variable, 
-    predicted y values, and the model used for the predictions.
-    """
-    r2 = model.score(x,y)
-    n = x.shape[0]
-    p = y.shape[1]
-    return 1 - (1 - r2) * (n - 1) / (n - p - 1)
-
-def prod_model_generator(prod_df, target_col):
-    """
-    Defines a series of steps that will preprocess data,
-    split data, and train a model for predicting actual productivity
-    using linear regression. It will return the trained model
-    and print the mean squared error, r-squared, and adjusted
-    r-squared scores.
-    """
-    # Create a list of steps for a pipeline that will one hot encode and scale data
-    # Each step should be a tuple with a name and a function
-    steps = [("Scale", StandardScaler(with_mean=False)), 
-        ("Linear Regression", LinearRegression())]
-
-    # Create a pipeline object
-    pipeline = Pipeline(steps)
-
-    # Apply the preprocess_rent_data step
-    X_train, X_test, y_train, y_test = preprocess_prod_data(prod_df, target_col)
-
-    # Fit the pipeline
-    pipeline.fit(X_train, y_train)
-
-    # Use the pipeline to make predictions
-    y_pred = pipeline.predict(X_test)
-
-    mse = mean_squared_error(y_test, y_pred)
-    r2_value = r2_score(y_test, y_pred)
-    r2_adj_value = r2_adj(X_test, y_test, pipeline)
-
-    # Print out the MSE, r-squared, and adjusted r-squared values
-    print(f"Mean Squared Error: {mse}")
-    print(f"R-squared: {r2_value}")
-    print(f"Adjusted R-squared: {r2_adj_value}")
-    if r2_adj_value < 0.4:
-        print("WARNING: LOW ADJUSTED R-SQUARED VALUE")
-
-    # Return the trained model
-    print(pipeline)
-    return pipeline
-
-def ridge_model_generator(prod_df, target_col):
-    """
-    Defines a series of steps that will preprocess data,
-    split data, and train a model for predicting actual productivity
-    using linear regression. It will return the trained model
-    and print the mean squared error, r-squared, and adjusted
-    r-squared scores.
-    """
-    # Create a list of steps for a pipeline that will one hot encode and scale data
-    # Each step should be a tuple with a name and a function
-    steps = [("Scale", StandardScaler(with_mean=False)), 
-             ("Ridge Linear Regression", Ridge(alpha=1))] 
-
-    # Create a pipeline object
-    pipeline = Pipeline(steps)
-
-    # Apply the preprocess_rent_data step
-    X_train, X_test, y_train, y_test = preprocess_prod_data(prod_df, target_col)
-
-    # Fit the pipeline
-    pipeline.fit(X_train, y_train)
-
-    # Use the pipeline to make predictions
-    y_pred = pipeline.predict(X_test)
-
-    mse = mean_squared_error(y_test, y_pred)
-    r2_value = r2_score(y_test, y_pred)
-    r2_adj_value = r2_adj(X_test, y_test, pipeline)
-
-    # Print out the MSE, r-squared, and adjusted r-squared values
-    print(f"Mean Squared Error: {mse}")
-    print(f"R-squared: {r2_value}")
-    print(f"Adjusted R-squared: {r2_adj_value}")
-    if r2_adj_value < 0.4:
-        print("WARNING: LOW ADJUSTED R-SQUARED VALUE")
-
-    # Return the trained model
-    print(pipeline)
-    return pipeline
-
 if __name__ == "__main__":
     print("This script should not be run directly! Import these functions for use in another file.")
-
-    
-
-
-
 
